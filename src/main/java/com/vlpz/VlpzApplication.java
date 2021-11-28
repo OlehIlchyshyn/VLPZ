1ee69408656428dd2bdaedb8a17a0502c1fbd6d9
Oleh Ilchyshyn <o.ilchyshyn01@gmail.com>
2021-11-28 19:58:37 +0200

    no pre-created data

--- src/main/java/com/vlpz/VlpzApplication.java
+++ src/main/java/com/vlpz/VlpzApplication.java
@@ -13,42 +13,7 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VlpzApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(VlpzApplication.class, args);
-	}
-
-	@Bean
-	public CommandLineRunner demoAdminAndStudent(AuthService authService,
-											  TaskService taskService,
-											  StatisticsService statisticsService,
-											  @Value("${spring.auth.admin.email}") String adminEmail,
-											  @Value("${spring.auth.admin.password}") String adminPassword,
-											  @Value("${spring.auth.student.email}") String studentEmail,
-											  @Value("${spring.auth.student.password}") String studentPassword) {
-		return args -> {
-			UserDto studentDto = new UserDto();
-			studentDto.setName("Student");
-			studentDto.setEmail(studentEmail);
-			studentDto.setPassword(studentPassword);
-			studentDto = authService.signUp(studentDto);
-			authService.signOut();
-
-			UserDto adminDto = new UserDto();
-			adminDto.setName("Admin");
-			adminDto.setEmail(adminEmail);
-			adminDto.setPassword(adminPassword);
-			authService.signUp(adminDto);
-
-			TaskDto taskDto = new TaskDto();
-			taskDto.setCaption("Example");
-			taskDto.setDescription("Test task");
-			taskDto.setComplexity("HARD");
-			taskDto.setSolutionDiagramJson("json");
-			taskDto = taskService.createTask(taskDto);
-
-			statisticsService.logAttempt(taskDto, studentDto, false, 2);
-			statisticsService.logAttempt(taskDto, studentDto, true, 35);
-		};
 	}
 }
