1fa7c07f0265a501db8d4469badbaa0333dafc3e
Oleh Ilchyshyn <o.ilchyshyn01@gmail.com>
2021-12-09 00:03:15 +0200

    cors disable

--- src/main/java/com/vlpz/config/WebConfig.java
+++ src/main/java/com/vlpz/config/WebConfig.java
@@ -4,12 +4,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@Configuration
+@Configuration(proxyBeanMethods = false)
 public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+        registry.addMapping("/**").allowedMethods("*").allowedHeaders("*");
     }
 }
