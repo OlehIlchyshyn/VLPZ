734c6cdf5e0871e0e838aa8519c1ec4af72c0592
Oleh Ilchyshyn <o.ilchyshyn01@gmail.com>
2021-12-08 23:53:06 +0200

    cors

--- src/main/java/com/vlpz/VlpzApplication.java
+++ src/main/java/com/vlpz/VlpzApplication.java
@@ -1,15 +1,7 @@
 package com.vlpz;
 
-import com.vlpz.dto.TaskDto;
-import com.vlpz.dto.UserDto;
-import com.vlpz.service.AuthService;
-import com.vlpz.service.StatisticsService;
-import com.vlpz.service.TaskService;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VlpzApplication {
--- src/main/java/com/vlpz/config/SecurityConfig.java
+++ src/main/java/com/vlpz/config/SecurityConfig.java
@@ -14,6 +14,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration()
 @EnableWebSecurity
@@ -52,8 +57,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
           .anyRequest().authenticated()
           .and()
         .cors()
-          .disable()
-        .csrf()
+//          .disable()
+//        .csrf()
           .disable()
         .httpBasic();
   }
@@ -61,6 +66,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    final CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(List.of("*"));
+    configuration.setAllowedMethods(List.of("HEAD",
+            "GET", "POST", "PUT", "DELETE", "PATCH"));
+    // setAllowCredentials(true) is important, otherwise:
+    // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
+    configuration.setAllowCredentials(true);
+    // setAllowedHeaders is important! Without it, OPTIONS preflight request
+    // will fail with 403 Invalid CORS request
+    configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
   }
 
 }
